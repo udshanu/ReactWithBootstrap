f33546f55a9fa29b7fc0c4433a7e6031c07eb712
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2020-06-24 21:56:23 +0530

    Add components.

--- demo-project/src/App.js
+++ demo-project/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from "./components/home/home";
+import { About } from "./components/about/about";
+import { Contact } from "./components/contact/contact";
+import { NoMatch } from "./components/nomatch/nomatch";
+import { Layout } from "./components/layout/layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Layout>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
+      </Layout>
+    </React.Fragment>
   );
 }
 
