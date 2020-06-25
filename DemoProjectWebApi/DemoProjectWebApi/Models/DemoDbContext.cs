f640a5362e98c103143e9c065574099a9ac0b144
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2020-06-25 22:12:13 +0530

    Create api project and database.

---
+++ DemoProjectWebApi/DemoProjectWebApi/Models/DemoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProjectWebApi.Models
+{
+    public class DemoDbContext : DbContext
+    {
+        public DemoDbContext(DbContextOptions<DemoDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DemoProjectWebApi/DemoProjectWebApi/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using DemoProjectWebApi.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProjectWebApi.Models
+{
+    public class User : Entity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [StringLength(100)]
+        public string Address { get; set; }
+        [StringLength(100)]
+        public string Address2 { get; set; }
+        [StringLength(100)]
+        public string Address3 { get; set; }
+        [StringLength(50)]
+        public string City { get; set; }
+        [StringLength(100)]
+        public string Email { get; set; }
+        [StringLength(50)]
+        public string Mobile { get; set; }
+        [StringLength(50)]
+        public string Tele { get; set; }
+        [StringLength(50)]
+        public string UserName { get; set; }
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
